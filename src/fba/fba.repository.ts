d202dc44a3e89532da50f08ead11236761c656cf
salmanpary <salmanpary@gmail.com>
2023-01-17 04:07:36 +0530

    fix:json path change

--- src/fba/fba.repository.ts
+++ src/fba/fba.repository.ts
@@ -4,37 +4,35 @@ import { join } from 'path';
 @Injectable()
 export class FbaRepository {
   async getReferralData() {
-    const data = await readFile(join(process.cwd(),'./src/fba/data/amazonFees/referralFee.json'), 'utf8');
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/amazonFees/referralFee.json', 'utf8');
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getFixedClosingFeeData() {
-    const data = await readFile(join(process.cwd(),
-      './src/fba/data/amazonFees/fixedClosingFee.json'),
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/amazonFees/fixedClosingFee.json',
       'utf8',
     );
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getVariableClosingFeeData() {
-    const data = await readFile(join(process.cwd(),
-      './src/fba/data/amazonFees/variableClosingFee.json'),
+    const data = await readFile(
+    join(__dirname, '..', 'public') + '/fba/data/amazonFees/variableClosingFee.json',
       'utf8',
     );
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getProductSizeTierData() {
-    const data = await readFile(join(process.cwd(),'./src/fba/data/fulfillmentFees/sizeTier.json'), 'utf8');
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/sizeTier.json', 'utf8');
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getFulfillmentFeeData() {
-    const data1 = await readFile(join(
-      process.cwd(),'./src/fba/data/fulfillmentFees/fulfillmentFee.json'),
+    const data1 = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/fulfillmentFee.json',
       'utf8',
     );
-    const data2 = await readFile(join(process.cwd(),'./src/fba/data/fulfillmentFees/addOn.json'), 'utf8');
+    const data2 = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/addOn.json', 'utf8');
     const dataObj1 = JSON.parse(data1);
     const dataObj2 = JSON.parse(data2);
     return {
@@ -43,26 +41,24 @@ export class FbaRepository {
     };
   }
   async getAddOnData() {
-    const data = await readFile(join(process.cwd(),'./src/fba/data/fulfillmentFees/addOn.json'), 'utf8');
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/addOn.json', 'utf8');
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getDimensionalWeightUsageData() {
-    const data = await readFile(join(process.cwd(),
-      './src/fba/data/fulfillmentFees/dimensionalWeightUsage.json'),
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/dimensionalWeightUsage.json',
       'utf8',
     );
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getApparelsDetails() {
-    const data = await readFile(join(process.cwd(),'./src/fba/data/fulfillmentFees/apparel.json'), 'utf8');
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/fulfillmentFees/apparel.json', 'utf8');
     const dataObj = JSON.parse(data);
     return dataObj;
   }
   async getMontlyChargeData() {
-    const data = await readFile(join(process.cwd(),
-      './src/fba/data/storageFees/monthlyCharge.json'),
+    const data = await readFile(join(__dirname, '..', 'public') + '/fba/data/storageFees/monthlyCharge.json',
       'utf8',
     );
     const dataObj = JSON.parse(data);
